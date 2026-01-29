a5f8c9aea6436a9accbff5c9dc878df2649b0ae8
Carlos Augusto <carlos.aug.developer@gmail.com>
2026-01-29 08:26:12 -0300

    fix: disabling next image otimization

--- next.config.ts
+++ next.config.ts
@@ -2,32 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: process.env.NODE_ENV === "development", // Desabilita otimização em dev
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "https",
-        hostname: "admin-noticias-dev.associacaojet.com.br",
-        pathname: "/uploads/**",
-      },
-      // Se você tiver um Strapi em produção, adicione também:
-      // {
-      //   protocol: "https",
-      //   hostname: "seu-strapi-producao.com",
-      //   pathname: "/uploads/**",
-      // },
-    ],
+    unoptimized: true, //process.env.NODE_ENV === "development", // Desabilita otimização em dev
+    // remotePatterns: [
+    //   {
+    //     protocol: "http",
+    //     hostname: "localhost",
+    //     port: "1337",
+    //     pathname: "/uploads/**",
+    //   },
+    //   {
+    //     protocol: "http",
+    //     hostname: "127.0.0.1",
+    //     port: "1337",
+    //     pathname: "/uploads/**",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "admin-noticias-dev.associacaojet.com.br",
+    //     pathname: "/uploads/**",
+    //   },
+    // ],
   },
 };
 
